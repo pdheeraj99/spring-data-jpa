41ceb973bcaffc8033f33d9bf802ef5be7031c6f
dheeraj <pilladheeraj@gmail.com>
2023-11-03 19:42:08 +0530

    ADDED REPO and Command Line Runner - Successfullly connected to DB

--- src/main/java/com/springlearn/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/springlearn/springdatajpa/SpringDataJpaApplication.java
@@ -1,13 +1,43 @@
 package com.springlearn.springdatajpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import com.springlearn.springdatajpa.entity.Student;
+import com.springlearn.springdatajpa.repository.StudentRepository;
 
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
+	@Autowired
+	private StudentRepository studentRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataJpaApplication.class, args);
+	}
+
+	// The MyCommandLineRunner class is a component in the Spring Data JPA
+	// application that implements the CommandLineRunner interface. It is
+	// responsible for executing code when the application starts up.
+	@Component
+	public class MyCommandLineRunner implements CommandLineRunner {
+
+		@Override
+		public void run(String... args) throws Exception {
+
+			Student student = Student.builder()
+					.emailId("dheeraj@gmail.com")
+					.firstName("dheeraj")
+					.lastName("kumar")
+					.guardianEmail("guardian1@gmail")
+					.guardianName("guardian1")
+					.guardingMobile("9999999999")
+					.build();
+			studentRepository.save(student);
+		}
 	}
 
 }
